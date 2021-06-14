f1e3c9e875dd30cb13872ae976f287f4c79032d3
Lobo <matheuscardoso1@poli.ufrj.br>
2021-06-14 12:38:35 -0300

    added theme switcher

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { ThemeProvider } from "styled-components"
 import { temaClaro, temaEscuro } from "./Components/UI/temas"
+import { BtnTema } from "./Components/UI";
 
 import Cabecalho from "./Components/Cabecalho";
 import Container from "./Components/Container";
 import {GlobalStyle} from "./Components/GlobalStyle";
+import SwitcherTema from "./Components/UI/SwitcherTema";
 
 function App() {
+  const [tema, setTema] = useState(true)
+  
+  const toggleTema = () => {
+    setTema((temaAtual) => !temaAtual)
+  }
+
   return (
     <>
-      <ThemeProvider theme = {temaEscuro}>
+      <ThemeProvider theme = {tema ? temaClaro : temaEscuro}>
         <GlobalStyle />
+        <BtnTema onClick = {toggleTema}>
+          <SwitcherTema tema={tema} />
+        </BtnTema>
         <Cabecalho />
         <Container />
       </ThemeProvider>
